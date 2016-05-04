938d3127971765df29f0d71b2d08af637ca4a926
Petra S Trulsson <petra.st@icloud.com>
2016-05-04 12:01:38 +0200

    Demo1
    Gamla demon med färgbytarfunktion

---
+++ demo1/demo.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+	
+	"use strict";
+	
+	$("#blue").click(function () {
+		$("body").css({backgroundColor: "#ccd9ff"});
+	});
+					 
+	$("#red").click(function () {
+		$("body").css({backgroundColor: "#ffb3b3"});
+	});
+					
+	$("#green").click(function () {
+		$("body").css({backgroundColor: "#ddffcc"});
+	});
+					  
+	$("#yellow").click(function () {
+		$("body").css({backgroundColor: "#fff0b3"});
+	});
+});
---
+++ demo1/demoold.js
@@ -0,0 +1,30 @@
+window.onload = background;
+
+function background() {
+    var a =
+document.getElementById("blue").onclick = setBlue;
+    
+    var a =
+document.getElementById("red").onclick = setRed;
+    
+    var a =
+document.getElementById("green").onclick = setGreen;
+
+var a =
+document.getElementById("yellow").onclick = setYellow; }
+
+function setBlue() {
+    var design =
+        document.body.style.backgroundColor =  "#ccd9ff"; } 
+
+function setRed() {
+    var design =
+        document.body.style.backgroundColor =  "#ffb3b3"; } 
+
+function setGreen() {
+    var design =
+        document.body.style.backgroundColor =  "#ddffcc"; } 
+
+function setYellow() {
+    var design =
+        document.body.style.backgroundColor =  "#fff0b3"; } 
